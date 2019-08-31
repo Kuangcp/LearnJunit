90687e1ec648affe26d24f6aecf62370b7806f30
Kuangcp <kuangcp@aliyun.com>
2019-09-01 00:10:02 +0800

    *) crlf -> lf

--- examples/src/main/java/junit/samples/AllTests.java
+++ examples/src/main/java/junit/samples/AllTests.java
@@ -4,18 +4,18 @@ import junit.framework.*;
 
 /**
  * TestSuite that runs all the sample tests
- *
  */
 public class AllTests {
 
-	public static void main (String[] args) {
-		junit.textui.TestRunner.run (suite());
-	}
-	public static Test suite ( ) {
-		TestSuite suite= new TestSuite("All JUnit Tests");
-		suite.addTest(VectorTest.suite());
-		suite.addTest(new TestSuite(junit.samples.money.MoneyTest.class));
-		suite.addTest(junit.tests.AllTests.suite());
-	    return suite;
-	}
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(suite());
+  }
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite("All JUnit Tests");
+    suite.addTest(VectorTest.suite());
+    suite.addTest(new TestSuite(junit.samples.money.MoneyTest.class));
+    suite.addTest(junit.tests.AllTests.suite());
+    return suite;
+  }
 }
--- examples/src/main/java/junit/samples/SimpleTest.java
+++ examples/src/main/java/junit/samples/SimpleTest.java
@@ -4,17 +4,18 @@ import junit.framework.*;
 
 /**
  * Some simple tests.
- *
  */
 public class SimpleTest extends TestCase {
-	protected int fValue1;
-	protected int fValue2;
 
-	protected void setUp() {
-		fValue1= 2;
-		fValue2= 3;
-	}
-	public static Test suite() {
+  protected int fValue1;
+  protected int fValue2;
+
+  protected void setUp() {
+    fValue1 = 2;
+    fValue2 = 3;
+  }
+
+  public static Test suite() {
 
 		/*
 		 * the type safe way
@@ -34,29 +35,33 @@ public class SimpleTest extends TestCase {
 		return suite;
 		*/
 
-		/*
-		 * the dynamic way
-		 */
-		return new TestSuite(SimpleTest.class);
-	}
-	public void testAdd() {
-		double result= fValue1 + fValue2;
-		// forced failure result == 5
-		assertTrue(result == 6);
-	}
-	public void testDivideByZero() {
-		int zero= 0;
-		int result= 8/zero;
-	}
-	public void testEquals() {
-		assertEquals(12, 12);
-		assertEquals(12L, 12L);
-		assertEquals(new Long(12), new Long(12));
+    /*
+     * the dynamic way
+     */
+    return new TestSuite(SimpleTest.class);
+  }
 
-		assertEquals("Size", 12, 13);
-		assertEquals("Capacity", 12.0, 11.99, 0.0);
-	}
-	public static void main (String[] args) {
-		junit.textui.TestRunner.run(suite());
-	}
+  public void testAdd() {
+    double result = fValue1 + fValue2;
+    // forced failure result == 5
+    assertTrue(result == 6);
+  }
+
+  public void testDivideByZero() {
+    int zero = 0;
+    int result = 8 / zero;
+  }
+
+  public void testEquals() {
+    assertEquals(12, 12);
+    assertEquals(12L, 12L);
+    assertEquals(new Long(12), new Long(12));
+
+    assertEquals("Size", 12, 13);
+    assertEquals("Capacity", 12.0, 11.99, 0.0);
+  }
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(suite());
+  }
 }
